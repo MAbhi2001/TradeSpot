63f3297f67e0189563eb981ec26313c129888261
Abhishek Mahajan <abhishek.mahajan@nitorinfotech.com>
2024-07-17 22:50:06 +0530

    first commit

---
+++ back-end/TradeSpotBackend/src/main/java/com/app/backend/TradeSpotBackendApplication.java
@@ -0,0 +1,22 @@
+package com.app.backend;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class TradeSpotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeSpotBackendApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper mapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		return modelMapper;
+	}
+}
