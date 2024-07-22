03943c6cab6a876a74b2a512dbc0b99f615967ab
Jayshri Jadhav <jayshri.jadhav@nitorinfotech.com>
2024-07-22 12:04:42 +0530

    updated

--- front-end/TradeSpotFrontend/src/app/app-routing.module.ts
+++ front-end/TradeSpotFrontend/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { RegisterComponent } from './components/register/register.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
